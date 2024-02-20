51e6845c90de9cdd874ae6d1e4e97defdc707033
Hamid Taheri <80408054+hamidthri@users.noreply.github.com>
2024-02-20 22:44:19 +0000

    done ex00 cpp03

---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,28 @@
+
+
+# ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap
+{
+
+    private:
+        std::string _name;
+        int _hitpoints;
+        int _energyPoints;
+        int _attackDamage;
+    
+    public:
+        ClapTrap();
+        ClapTrap(std::string name);
+        ~ClapTrap();
+        void attack(std::string const & target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+
+        ClapTrap & operator=(ClapTrap const & rhs);
+};
+
+#endif
