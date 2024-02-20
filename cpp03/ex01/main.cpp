5493f547fa362f54e58746450c6aa27e2cc51365
Hamid Taheri <taheri.hamiid@gmail.com>
2024-02-20 23:52:17 +0100

    ex01 added

---
+++ cpp03/ex01/main.cpp
@@ -0,0 +1,13 @@
+
+# include "ClapTrap.hpp"
+int main()
+{
+    ClapTrap claptrap("ClapTrap");
+    ClapTrap claptrap2("ClapTrap2");
+
+    claptrap.attack("ClapTrap2");
+    claptrap2.takeDamage(5);
+    claptrap2.beRepaired(5);
+
+    return 0;
+}
